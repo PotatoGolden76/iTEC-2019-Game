c81ed0043910eaad43eb94e562b195b789677164
Harangus Robert <potatogolden76@gmail.com>
2019-11-23 16:23:56 +0200

    V0.2
    Player prefabs done
    Logo added
    Tileset added
    misc sprites added

--- iTEC 2019/Assets/Scripts/PlayerController.cs
+++ iTEC 2019/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour
     public int speed;
     public float checkRad;
     private float movement;
+    private float scl;
+    private bool isRight;
+
+    [SerializeField]
+    private Animator anim;
 
     public LayerMask GroundLayer;
 
@@ -22,7 +27,10 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        scl = transform.localScale.x;
+        isRight = true;
     }
 
     // Update is called once per frame
@@ -30,15 +38,45 @@ public class PlayerController : MonoBehaviour
     {
         if(isGrounded && Input.GetKeyDown(jumpKey)) {
             rb.velocity = Vector2.up * jumpForce;
+            anim.SetTrigger("Jumping");
+            //anim.SetBool("InAir", true);
+        }
+
+        if (movement == -1 && isRight)
+        {
+            transform.localScale = new Vector3(scl * -1, transform.localScale.y, 1);
+            isRight = false;
+        }
+        else if (movement == 1 && !isRight)
+        {
+            transform.localScale = new Vector3(scl, transform.localScale.y, 1);
+            isRight = true;
+        }
+
+        if(movement != 0 && isGrounded)
+        {
+            anim.SetBool("Running", true);
+        } else
+        {
+            anim.SetBool("Running", false);
         }
     }
 
     void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRad, GroundLayer);
+        if(isGrounded)
+        {
+            anim.SetBool("InAir", false);
+        } else
+        {
+            anim.SetBool("InAir", true);
+        }
 
         movement = Input.GetAxisRaw(inputAxis);
 
         rb.velocity = new Vector2(movement * speed, rb.velocity.y);
+
+        
     }
 }
